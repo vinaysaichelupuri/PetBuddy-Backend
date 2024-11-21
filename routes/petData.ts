311449143cf0696c55ce003f72fe0a54471a1f44
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2024-11-21 17:31:01 +0530

    Implement the pet data route

---
+++ routes/petData.ts
@@ -0,0 +1,17 @@
+import { PetData } from "../services/petData";
+import express, { Request, Response } from 'express';
+const router = express.Router(); 
+router.post('/', async (req: Request, res: Response) => {
+  try {
+    const { username} = req.body;
+    const user = new PetData(username)
+    const results =await user.getPetData();
+    console.log(results)
+    if (results) {
+        res.status(200).json(results)
+}
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+export default router;
