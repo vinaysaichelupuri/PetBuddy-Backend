ea0d0246eec3f4e5c76468e673160e8ed1f45a44
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2024-11-21 18:56:26 +0530

    Implement the route for add activity

---
+++ routes/addActivity.ts
@@ -0,0 +1,24 @@
+import { AddActivity } from "../services/addActivity";
+import express, { Request, Response } from 'express';
+const router = express.Router(); 
+router.post('/', async (req: Request, res: Response) => {
+  try {
+    const { username,petName,type,remainderName,date,startTime,endTime} = req.body;
+    const user = new AddActivity(username,petName,remainderName,date,startTime,endTime)
+    const results =await user.addActivity();
+    if (results) {
+    if(results.status===200){
+        res.status(results.status).json({message: 'Added Reminder successful'});
+    }
+    if(results.status===404){
+        res.status(results.status).json({message: 'User not exist'});
+    }
+    if(results.status===401){
+        res.status(results.status).json({message: 'No pet found'});
+    }
+}
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+export default router;
