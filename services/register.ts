82116c57a7bfa90dbb08a2877b13b0e020985860
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2024-11-21 16:04:14 +0530

    Implement the register functionality

--- services/register.ts
+++ services/register.ts
@@ -1,25 +1,30 @@
 import { data } from "../models/data";
 export class Register {
-    username:string;
-    password:string;
-    userPhoto:string;
-    constructor(username:string,password:string,userPhoto:string){
-        this.username = username,
-        this.password = password,
-        this.userPhoto =userPhoto
+  username: string;
+  password: string;
+  userPhoto: string;
+  email: string;
+  phoneNumber: number;
+  constructor(
+    username: string,
+    password: string,
+    userPhoto: string,
+    email: string,
+    phoneNumber: number
+  ) {
+    (this.username = username),
+      (this.password = password),
+      (this.userPhoto = userPhoto),
+      (this.email = email),
+      (this.phoneNumber = phoneNumber);
+  }
+  async getRegister() {
+    const user = await data.findOne({ username: this.username });
+    if (user) {
+      return { status: 400 };
+    } else {
+      data.create({username:this.username,password:this.password,userPhoto:this.userPhoto,email:this.email,phoneNumber:this.phoneNumber})
+      return { status: 200 };
     }
-    async getRegister(){
-        const username = this.username
-        const password = this.password
-        const userPhoto = this.userPhoto
-        const user = await data.findOne({username:username})
-        if(user){
-            return {status:400}
-        }
-        if(username===''||password===''){
-            return {status:401}
-        }
-        const newUser = new data({ username, password, userPhoto });
-        await newUser.save();
-    }
+  }
 }
