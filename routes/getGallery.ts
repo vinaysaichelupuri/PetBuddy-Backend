2554b536fb198a811522e94fd5cb933bad1a26c5
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2024-11-22 01:19:55 +0530

    Implement the route for getting gallery images

---
+++ routes/getGallery.ts
@@ -0,0 +1,25 @@
+import { GetGallery } from "../services/getGallery";
+import express, { Request, Response } from 'express';
+const router = express.Router(); 
+router.post('/', async (req: Request, res: Response) => {
+  try {
+    const { username,petName} = req.body;
+    const user = new GetGallery(username,petName)
+    const results =await user.getGallery();
+    console.log(results.petGallery)
+    if (results) {
+    if(results.status===200){
+        res.status(results.status).json(results.petGallery);
+    }
+    if(results.status===404){
+        res.status(results.status).json({message: 'User not exist'});
+    }
+    if(results.status===401){
+        res.status(results.status).json({message: 'Pet not exist'});
+    }
+}
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+export default router;
