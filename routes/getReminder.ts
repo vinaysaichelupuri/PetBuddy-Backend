eae44e0272f87bfa8f4da9888b0f7757c1baa739
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2024-11-22 01:35:50 +0530

    Implement the route for getting reminder

---
+++ routes/getReminder.ts
@@ -0,0 +1,24 @@
+import { GetReminder } from "../services/getReminder";
+import express, { Request, Response } from 'express';
+const router = express.Router(); 
+router.post('/', async (req: Request, res: Response) => {
+  try {
+    const { username,petName} = req.body;
+    const user = new GetReminder(username,petName)
+    const results =await user.getReminder();
+    if (results) {
+    if(results.status===200){
+        res.status(results.status).json(results.petReminder);
+    }
+    if(results.status===404){
+        res.status(results.status).json({message: 'User not exist'});
+    }
+    if(results.status===401){
+        res.status(results.status).json({message: 'Pet not exist'});
+    }
+}
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+export default router;
